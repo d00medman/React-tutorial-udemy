418008a209ae2add48676b7b5c6104cad749ce7c
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 20:45:21 -0400

    lesson 14; first component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAETgxJVXH3kDDwbHc_FcrUj0mRxTTMDAE';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return(
+  <div>
+    <SearchBar />
+  </div>
+  )
 }
 
 // Instances of components called with jsx tags
