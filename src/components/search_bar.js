126b16562fc7ef29401ea4312d72d8614ce380b6
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 20:56:02 -0400

    lesson 16; event handling in components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 // This is a class based component, rather than simply being rendered by a function
 // class based components extend react components
 class SearchBar extends Component {
+  // Note the curly braces, which are how jsx handles variables
   render() {
-    return <input />
+    return <input onChange={event => console.log(event.target.value)}/>
   }
+
 }
 
 export default SearchBar;
