d9ce8edd9b4d65867937c0d1a5c40ae3ccea0b8d
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 20:50:50 -0400

    lesson 15; functional and class based components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+// This is a class based component, rather than simply being rendered by a function
+// class based components extend react components
+class SearchBar extends Component {
+  render() {
+    return <input />
+  }
+}
 
 export default SearchBar;
