44bf43485daef904780300f91fbc140a5263c9c1
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 21:17:52 -0400

    lesson 21; rudimentary ajax get call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAETgxJVXH3kDDwbHc_FcrUj0mRxTTMDAE';
+
+YTSearch({key:API_KEY, term:'surfing'}, function(data) {
+  console.log(data)
+})
 
 const App = () => {
   return(
