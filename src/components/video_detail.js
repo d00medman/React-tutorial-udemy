7586eae735685b5f2d793d54d2f54878a66af915
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 21:56:57 -0400

    lesson 27; displaying video

---
+++ src/components/video_detail.js
@@ -0,0 +1,25 @@
+// When making a component, when decideing whether to go class or go functional,
+// the real deciding factor should be whether or not it needs to use any state
+  // This is an aspect which i was missing in r-r, because I was using the generator,
+  // I pretty much never used functional components
+import React from 'react';
+
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId
+  const url = `https://www.youtube.com/embed/${videoId}`
+
+  return (
+    <div className="video detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoDetail
