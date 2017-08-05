77d565434766128c147cf424583d4afb12cb503a
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 21:39:30 -0400

    lesson 25; adding keys to list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,8 @@ import VideoListItem from './video_list_item'
 // Note that props needed to be passed through the video list function in order to work
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video}/>
+    // The key is used to maintain lists; a logical structure used to make things easier to render
+    return <VideoListItem key={video.etag} video={video}/>
   })
   // well shit, even components use the className, a lot like react-rails
   // lol, as it turns out, this.props is in fact how class-based components handle props, a lot like r-r
