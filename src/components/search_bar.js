c5a082e5ea464eac7805646722a8cfe73f7a9e79
AlexMollohan <ajmollohan@gmail.com>
2017-08-04 20:59:27 -0400

    lesson 17; introduction to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 // This is a class based component, rather than simply being rendered by a function
 // class based components extend react components
 class SearchBar extends Component {
+  // Each component has its own state, and its changes re-render a component and all child
+  // components. State is set in the constructor
+  constructor(props){
+    // this super method allows a component to inherit all the props of its parent component
+    super(props);
+
+    this.state = { term: ''};
+  }
   // Note the curly braces, which are how jsx handles variables
   render() {
     return <input onChange={event => console.log(event.target.value)}/>
   }
 
 }
+
+
 
 export default SearchBar;
